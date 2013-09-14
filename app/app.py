3c48718c1e87894137c2b6be4c90efc3662be074
Andy Russell <arussell123@gmail.com>
2013-09-13 19:28:43 -0700

    revise api

--- app/app.py
+++ app/app.py
@@ -1,10 +1,25 @@
 from flask import *
 
+import json
+
+import db_interface
+
 app = Flask(__name__)
 
-@app.route('/shows', methods=['POST'])
-def add_show():
-    pass
+@app.route('/user/<int:user_id>')
+def front_page_json(user_id):
+    front_page = {}
+    front_page['recent_shows'] = db_interface.get_recent_shows(user_id)
+    return json.dumps(front_page)
+
+@app.route('/media', methods=['POST'])
+def mark_media_watched():
+    media_type = request.form['type']
+    title = request.form['title']
+    if media_type == 'tv':
+        season = int(request.form['season'])
+        episode = int(request.form['episode'])
+        db_interface.mark_tv_watched(title, season, episode)
 
 if __name__ == '__main__':
     app.run()
