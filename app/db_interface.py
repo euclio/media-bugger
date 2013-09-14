20855360472d580b0d0e5ceab627c3a60a83c90b
AC <chen.alex121@gmail.com>
2013-09-13 20:08:08 -0700

    Updated Media.

--- app/db_interface.py
+++ app/db_interface.py
@@ -1,20 +1,43 @@
 import pymongo import MongoClient
 
 class db_interface:
-  def __init__(self,ClientName)
+  def error():
+    return {"ERROR": True}
+  def __init__(self,UserName):
     #Creates a Collection for Client also create a interface to db.
+    self.db = MongoClient('localhost',27017)["MBuggerDB"]
+    if UserName not in self.db.collections_names(include_system_collections=False):
+      self.db.create_collection(UserName)
+    self.user_data = self.db[UserName]
+    if "media" not in self.db.collection_names(include_system_collections=False):
+      self.db.create_collection("media")
+    self.media = self.db["media"]
     
     
     
-    #Show
-  def get_media(self, Title)
+    #Media
+  def get_media(self, Title):
     #get a media from the media collection.
+    ans = self.media.find_one({"Title":Title})
+    if ans == None:
+      return {"ERROR": True, "Cause": "Given Media Not Found:" + Title}
+    return ans
+    
   def add_media(self, Title, Summ, Type, Links = [])
     #add a media to the media collection.
+    self.media.insert({"Title":Title,"Summ":Summ,"Type":Type,"Links":Links})
+    
   def add_link_media(self, Title, Links)
     #add link to the media.  This is mainly for non TV shows items such as books and movies.
-  def seen_media(self, Title) 
-    #Edit the user's seend media and update the time.
+    target = self.media.find_one("Title")
+    if target == None:
+      return {"ERROR": True, "Cuase": "Given Media Not Found:" + Title}
+    target["Links"] += Links
+    self.media.update({'_id':target['_id']},target)
+    
+  def seen_media(self, Title)
+    #Edit the user's seen media and update the time.
+    
     
     
     #Episode Only for TV Shows
