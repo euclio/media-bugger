33330850799871c6436f27e561a621b24d1ebcff
Alex X Chen <axcqwerty@gmail.com>
2013-09-14 00:15:44 -0700

    Now loads FB data into localStorage after authentication

--- extension/src/browser_action/facebook.js
+++ extension/src/browser_action/facebook.js
@@ -1,15 +1,20 @@
 if (localStorage.accessToken) {
     var mainUrl = "https://graph.facebook.com/me?" + localStorage.accessToken;
     var friendsUrl = "https://graph.facebook.com/me/friends?" + localStorage.accessToken;
-    console.log(mainUrl);
-    console.log(friendsUrl);
 
     function processMain(data) {
-        console.log(data);
+        localStorage.first_name = data.first_name;
+        localStorage.middle_name = data.middle_name;
+        localStorage.last_name = data.last_name;
+        localStorage.fb_id = data.id;
     }
 
     function processFriends(data) {
-        console.log(data);
+        var friendsArray = [];
+        for (var i = 0; i < data.data.length; i++) {
+            friendsArray.push(data.data[i].id);
+        }
+        localStorage.friends = JSON.stringify(friendsArray);
     }
 
     $.get(mainUrl, {}, processMain);
