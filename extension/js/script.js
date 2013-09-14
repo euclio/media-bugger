ee43c394c6f1e25cadc77b67488d101dea9217e9
Don Yu <dy2212@columbia.edu>
2013-09-14 11:59:32 -0700

    will this script work

--- extension/js/script.js
+++ extension/js/script.js
@@ -1,45 +1,37 @@
-var getTVString = function(tab) {
-    var domain = tab.url.split('.')[1];
-    var tvName = 'Unable to parse TV show name';
-    var ready = false;
+// var getTVString = function() {
+//     var domain = tab.url.split('.')[1];
+//     var tvName = 'Unable to parse TV show name';
 
-    if (domain == 'youtube') {
-        tvName = tab.title;
-        ready = true;
-    } else if (domain == 'free-tv-video-online') {
-        chrome.tabs.executeScript(tab.id, {file: '../js/projectvCS.js'});
-        chrome.runtime.onMessage.addListener(
-                function(request, sender, sendResponse) {
-                    if (request.name) {
-                        tvName = request.name;
-                    } else {
-                        tvName = 'not working :(';
-                    }
-                    ready = true;
-                }
-        );
-    } else {
-        ready = true;
-    }
-    
-    while (!ready) {};
-    return tvName;
-};
+//     if (domain == 'youtube') {
+//         tvName = tab.title;
+//         ready = true;
+//     } else if (domain == 'free-tv-video-online') {
+//         var tvName = document.querySelectorAll("td div h1")[0].innerText;
+//         console.log(tvName);
+//     } 
+// };
 
-var parseTVNameParts = function(tvString) {
-    var showName;
-    var curSeason;
-    var curEpisode;
+// var parseTVNameParts = function(tvString) {
+//     var showName;
+//     var curSeason;
+//     var curEpisode;
 
-    return { showName : tvString, 
-             season : 4,
-             curEpisode : 2 };
-};
+//     return { showName : tvString, 
+//              season : 4,
+//              curEpisode : 2 };
+// };
 
 $(document).ready(function() {
     chrome.tabs.query({active:true, currentWindow:true}, function(tabArray) {
-        var tvString = getTVString(tabArray[0]);
-        console.log(tvString);
-        document.getElementById('video-name').innerHTML = tvString;
+        // var tvString = getTVString(tabArray[0]);
+        // console.log(tvString);
+        document.getElementById('video-name').innerHTML = 'Nothing';
     });
+    // document.getElementById('video-name').innerHTML = 'Nothing';                 
+    // chrome.extension.sendRequest({message:'getTV', data: 'hi'}, function(response) {});
 });
+
+// $(document).ready(function() {
+    // console.log('hello');
+// });
+
