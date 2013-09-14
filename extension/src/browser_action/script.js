fe640dd46cbbab42fc6f6e95cc82df031fe34185
Jiexi Cao <jcao@hmc.edu>
2013-09-13 21:23:33 -0700

    Added jquery stuff, need to fix the code to work with the activeTab

--- extension/src/browser_action/script.js
+++ extension/src/browser_action/script.js
@@ -1,14 +1,17 @@
 var getTVShowName = function() {
-    var domain = document.domain.split('.')[1];
+    var domain = activeTab.url;
+    console.log(domain);
     var tvName;
     if (domain == 'youtube') {
         tvName = $('title').text;
+        console.log("On youtube");
     } else if (domain == 'free-tv-video-online') {
         tvName = document.querySelectorAll('td div h1')[0].innerText;
+        console.log("On project free tv");
     }
-    return tvName == undefined ? tvName : 'Unable to parse TV show name';
+    return tvName != undefined ? tvName : 'Unable to parse TV show name';
 };
 
-$(document).ready() {
+$(document).ready(function() {
     $('#video-name').text(getTVShowName());
-};
+});
