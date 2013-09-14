060bcda757065313b073b56db4682ffde5ecd9f6
Alex X Chen <axcqwerty@gmail.com>
2013-09-14 12:37:46 -0700

    Reorganize

--- extension/src/bg/background.js
+++ extension/src/bg/background.js
@@ -12,15 +12,18 @@ chrome.extension.onMessage.addListener(
     sendResponse();
   });
 
+// https://github.com/zuzara/facebook-connect-for-chrome-extension
 var successURL = 'https://www.facebook.com/connect/login_success.html';
 function onFacebookLogin() {
     chrome.tabs.getAllInWindow(null, function(tabs) {
         for (var i = 0; i < tabs.length; i++) {
-            if (tabs[i].url.indexOf(successURL) == 0) {
+            if (tabs[i].url.indexOf(successURL) === 0) {
                 var params = tabs[i].url.split('#')[1];
                 access = params.split('&')[0]
                 localStorage.accessToken = access;
                 chrome.tabs.remove(tabs[i].id);
+                alert(localStorage.accessToken);
+                getLoginData();
                 return;
             }
         }   
