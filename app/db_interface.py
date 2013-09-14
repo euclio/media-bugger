fae0cc3a6ff9eb3bab45c4c00d46932f347a9c74
AC <chen.alex121@gmail.com>
2013-09-13 19:42:38 -0700

    included minimal API for db_interface

--- app/db_interface.py
+++ app/db_interface.py
@@ -1,7 +1,28 @@
 import pymongo import MongoClient
 
 class db_interface:
-  def __init__(self):
-    self.client = MongoClient('localhost',27017)
-    self.db = self.client.MBuggerDB
-  def getShow(self,Title):
+  def __init__(self,ClientName)
+    #Creates a Collection for Client also create a interface to db.
+    
+    
+    
+    #Show
+  def get_media(self, Title)
+    #get a media from the media collection.
+  def add_media(self, Title, Summ, Type, Links = [])
+    #add a media to the media collection.
+  def add_link_media(self, Title, Links)
+    #add link to the media.  This is mainly for non TV shows items such as books and movies.
+  def seen_media(self, Title) 
+    #Edit the user's seend media and update the time.
+    
+    
+    #Episode Only for TV Shows
+  def get_episode(self,Title)
+    #get the episode
+  def add_episode(self,Title, Summ, Season, Number, Links = [])
+    #return bool
+  def add_link_episode(self, Title)
+    #add link to the episode.
+    
+
